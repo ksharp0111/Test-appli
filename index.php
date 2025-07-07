d11f95392b82a08cdf111b23d7b05276a591f9a5
ksharp <armstrxng14@gmail.com>
2025-07-07 23:00:27 +0200

    update

--- index.php
+++ index.php
@@ -7,60 +7,22 @@
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Calendrier & Planification - Base</title>
-    <!-- Google Fonts: Poppins -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet" />
-    <!-- Google Material Icons -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-
 <body>
-   <nav>
-  <div class="container">
-    <a href="#" id="brand">Brand</a>
-    <button>
-      <span></span>
-      <span></span>
-      <span></span>
-    </button>
-    
-    <ul class="navbar-menu">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">page a</a></li>
-      <li><a href="#">page b</a></li>
-      <li><a href="#">page c</a></li>
-      <li><a href="#">page d</a></li>
-    </ul>
-    
-  </div>
-</nav>
+  
+  <header>
+    <a class="head" href="index.php"><h1><img class="logo" src="IMG/logo.png">GameFRESH</h1></a>
+  </header>
+ <nav>
+      <a href="">Home</a>
+      <a href="">About</a>
+      <a href="">Gallery</a>
+      <a href="">Contact</a>
+      <input type="navbar" placeholder="search...">
+ </nav>
 
-    <script>
-       $(document).ready(function () {
-  
-  'use strict';
-  
-   var c, currentScrollTop = 0,
-       navbar = $('nav');
-
-   $(window).scroll(function () {
-      var a = $(window).scrollTop();
-      var b = navbar.height();
-     
-      currentScrollTop = a;
-     
-      if (c < currentScrollTop && a > b + b) {
-        navbar.addClass("scrollUp");
-      } else if (c > currentScrollTop && !(a <= b)) {
-        navbar.removeClass("scrollUp");
-      }
-      c = currentScrollTop;
-     
-     console.log(a);
-  });
-  
-});
-    </script>
+    
 </body>
 </html>
 
